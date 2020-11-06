5a69a26605fcd31eb62c3e11a80faae81fd1299b
shivanshusingla <shivanshusingla82@gmail.com>
2020-11-06 09:59:27 +0530

    Added changes

--- src/main/resources/featurefiles/grofersAssignment.feature
+++ src/main/resources/featurefiles/grofersAssignment.feature
@@ -2,3 +2,5 @@ Feature: Grofers Assignment
 
   Scenario: All the users of City Grofers complete more than half of their todo tasks.
     Given User has the todo tasks
+    And User belongs to the city Grofers
+    Then User Completed task percentage should be greater than 50%
--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -3,6 +3,7 @@ package steps;
 import apiutils.ApiConfig;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 
 public class TestSteps {
 
@@ -13,7 +14,12 @@ public class TestSteps {
 
   @And("^User belongs to the city (.*)$")
   public void getUsersApiResponse(String cityName) {
-    ApiConfig.getResponse("http://jsonplaceholder.typicode.com/todos", "", "");
+    ApiConfig.getResponse("http://jsonplaceholder.typicode.com/users", "", cityName);
+  }
+
+  @Then("^User Completed task percentage should be greater than (.*)%$")
+  public void verifyUserTaskPercentage(String taskPercentage) {
+    ApiConfig.getResponse("http://jsonplaceholder.typicode.com/todos", "", "completed");
   }
 
 }
